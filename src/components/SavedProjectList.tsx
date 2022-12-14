b7b52454f88f44aeb3ff5b1212b984b733edf118
newt239 <ysti2019.win2020.st@gmail.com>
2022-12-14 18:33:13 +0900

    add: delete project

--- src/components/SavedProjectList.tsx
+++ src/components/SavedProjectList.tsx
@@ -1,4 +1,14 @@
-import { Card, H2, H3, Text } from "@blueprintjs/core";
+import { useState } from "react";
+
+import {
+  Button,
+  Card,
+  Classes,
+  H2,
+  H3,
+  Overlay,
+  Text,
+} from "@blueprintjs/core";
 import { useLiveQuery } from "dexie-react-hooks";
 import moment from "moment";
 import Link from "next/link";
@@ -6,14 +16,33 @@ import Link from "next/link";
 import { db } from "@/db";
 
 const SavedProjectList: React.FC = () => {
+  const [projectId, setProjectId] = useState<string | null>(null);
   const projects = useLiveQuery(async () => db.projects.toArray()) || [];
+
+  const deleteProject = (projectId: string) => {
+    db.projects.delete(projectId);
+    setProjectId(null);
+  };
+
   return (
     <div>
       <H2>保存済みのプロジェクト</H2>
       {projects && projects.length !== 0 ? (
         projects.map((project) => (
-          <Card key={project.id}>
-            <H3>{project.name}</H3>
+          <Card key={project.id} style={{ margin: "1rem" }}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <H3>{project.name}</H3>
+              <Button
+                icon="delete"
+                outlined
+                onClick={() => setProjectId(project.id)}
+              />
+            </div>
             <Text>{moment(project.createdAt).format("MM/DD HH:mm")}更新</Text>
             <Link href={`/project/${project.id}/studio`}>開く</Link>
           </Card>
@@ -21,6 +50,17 @@ const SavedProjectList: React.FC = () => {
       ) : (
         <Text>保存済みのプロジェクトはありません。</Text>
       )}
+      <Overlay isOpen={!!projectId} onClose={() => setProjectId(null)}>
+        <div className={Classes.CARD}>
+          <H3>プロジェクト 「{projectId}」 を削除しますか？</H3>
+          <Button
+            intent="danger"
+            onClick={() => deleteProject(projectId as string)}
+          >
+            削除
+          </Button>
+        </div>
+      </Overlay>
     </div>
   );
 };
