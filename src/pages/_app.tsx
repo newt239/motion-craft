d894b1dd087df64d9504ac86d77ea497b4b2529e
newt239 <ysti2019.win2020.st@gmail.com>
2022-12-23 15:23:51 +0900

    chore: wrap root component with MantineProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
+import { MantineProvider } from "@mantine/core";
 import { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
 import "#/global.scss";
-import dynamic from "next/dynamic";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const CSR = dynamic(() => import("#/components/CSRInner"), { ssr: false });
   return (
-    <CSR>
-      <Component {...pageProps} />
-    </CSR>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <CSR>
+        <Component {...pageProps} />
+      </CSR>
+    </MantineProvider>
   );
 }
 
